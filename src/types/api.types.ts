b6c9302cf84852438322ef6d30d6b6639aac4341
ludwig <ludwigstridh@gmail.com>
2025-11-04 10:11:15 +0100

    refactored ProfilePage, moved out types and created userApi for getting user profile

---
+++ src/types/api.types.ts
@@ -0,0 +1,12 @@
+export interface UserProfile {
+  username: string;
+  email: string;
+  registration: string[];
+}
+
+export interface Meetup {
+  id: string;
+  title: string;
+  date: string[];
+  description: string;
+}
