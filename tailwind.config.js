529cb988fd11c63897edebc48cbff2a0c9c2d6a0
Lilla Mry <maria.kjellholm@gmail.com>
2025-11-06 10:06:09 +0100

    erased unnecessary comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Tala om för Tailwind vilka filer den ska skanna efter klassnamn
+ 
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // Skannar alla dina komponent-filer
+    './src/**/*.{js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {},
